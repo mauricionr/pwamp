f0ba073824183bd7b3c5ba0632471ec0c3db4136
mauricionr <mauricioskateboard@gmail.com>
2017-06-25 01:32:44 -0300

    autocomplete working

---
+++ data/data.js
@@ -0,0 +1,562 @@
+module.exports = {
+    "skuToSizeAndPrice": {
+        "1001": {
+            "sizes": {
+                "XS": 8.99,
+                "S": 9.99
+            }
+        },
+        "1002": {
+            "sizes": {
+                "S": 10.99,
+                "M": 12.99,
+                "L": 14.99
+            }
+        },
+        "1010": {
+            "sizes": {
+                "L": 11.99,
+                "XL": 13.99
+            }
+        },
+        "1014": {
+            "sizes": {
+                "M": 7.99,
+                "L": 9.99,
+                "XL": 11.99
+            }
+        },
+        "1015": {
+            "sizes": {
+                "XS": 8.99,
+                "S": 10.99,
+                "L": 15.99
+            }
+        },
+        "1016": {
+            "sizes": {
+                "S": 8.99,
+                "L": 14.99,
+                "XL": 11.99
+            }
+        },
+        "1021": {
+            "sizes": {
+                "XS": 8.99,
+                "S": 9.99,
+                "M": 12.99
+            }
+        },
+        "1030": {
+            "sizes": {
+                "M": 10.99,
+                "L": 11.99
+            }
+        }
+    },
+    "items": [{
+            "_id": "594ec1ffeaa8b8a087372f00",
+            "index": 0,
+            "guid": "a9fa0123-ed16-4ccd-b600-f4a5f4f4257e",
+            "isActive": false,
+            "balance": "$3,564.39",
+            "picture": "http://placehold.it/32x32",
+            "age": 30,
+            "eyeColor": "green",
+            "name": {
+                "first": "Knight",
+                "last": "Foster"
+            },
+            "company": "SENTIA",
+            "email": "knight.foster@sentia.biz",
+            "phone": "+1 (982) 527-3909",
+            "address": "777 Roder Avenue, Woodlake, Connecticut, 6875",
+            "about": "Excepteur cupidatat laboris dolore proident labore esse magna ipsum. Laborum dolore veniam labore mollit sit enim pariatur sunt. Magna amet exercitation aliquip proident exercitation adipisicing anim sit cupidatat esse culpa ea incididunt. Qui culpa et amet do esse cillum aute officia cillum officia consequat aute magna. Nostrud commodo ea et cupidatat magna deserunt. Sit dolor adipisicing pariatur nisi aliqua Lorem adipisicing magna laboris nulla voluptate tempor. Adipisicing pariatur ex eiusmod veniam esse est eiusmod nostrud.",
+            "registered": "Sunday, October 11, 2015 9:09 PM",
+            "latitude": "-53.060431",
+            "longitude": "-164.028768",
+            "tags": [
+                "laborum",
+                "culpa",
+                "minim",
+                "culpa",
+                "consequat"
+            ],
+            "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            ],
+            "friends": [{
+                    "id": 0,
+                    "name": "Priscilla Reyes"
+                },
+                {
+                    "id": 1,
+                    "name": "Harding Petersen"
+                },
+                {
+                    "id": 2,
+                    "name": "Atkins Holcomb"
+                }
+            ],
+            "greeting": "Hello, Knight! You have 7 unread messages.",
+            "favoriteFruit": "strawberry"
+        },
+        {
+            "_id": "594ec1ff98a0c7db5e16986c",
+            "index": 1,
+            "guid": "74f4e0d7-420d-49a8-b206-5ac02ca47134",
+            "isActive": false,
+            "balance": "$2,813.65",
+            "picture": "http://placehold.it/32x32",
+            "age": 36,
+            "eyeColor": "green",
+            "name": {
+                "first": "Montoya",
+                "last": "Stephenson"
+            },
+            "company": "LOTRON",
+            "email": "montoya.stephenson@lotron.me",
+            "phone": "+1 (922) 515-2674",
+            "address": "900 Barlow Drive, Chelsea, Missouri, 2824",
+            "about": "Ullamco ut minim cupidatat esse occaecat amet. Voluptate anim amet dolor consequat nulla pariatur adipisicing laboris ipsum amet. Nisi et mollit nostrud consequat labore do adipisicing magna cillum magna ad amet. Laboris duis velit ipsum id elit magna. Eu proident laborum excepteur occaecat. Ea adipisicing est consectetur ipsum. Sint do do fugiat ut laboris Lorem quis velit qui et laboris irure.",
+            "registered": "Tuesday, June 6, 2017 2:08 AM",
+            "latitude": "5.85088",
+            "longitude": "61.028106",
+            "tags": [
+                "aute",
+                "quis",
+                "anim",
+                "excepteur",
+                "elit"
+            ],
+            "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            ],
+            "friends": [{
+                    "id": 0,
+                    "name": "Darla Webster"
+                },
+                {
+                    "id": 1,
+                    "name": "Hanson Fields"
+                },
+                {
+                    "id": 2,
+                    "name": "Blackburn Wynn"
+                }
+            ],
+            "greeting": "Hello, Montoya! You have 8 unread messages.",
+            "favoriteFruit": "strawberry"
+        },
+        {
+            "_id": "594ec1fffd55a60228b37920",
+            "index": 2,
+            "guid": "b58172db-91c5-4011-beba-24b511dac5f4",
+            "isActive": false,
+            "balance": "$2,051.65",
+            "picture": "http://placehold.it/32x32",
+            "age": 30,
+            "eyeColor": "blue",
+            "name": {
+                "first": "Hamilton",
+                "last": "Gonzales"
+            },
+            "company": "FARMAGE",
+            "email": "hamilton.gonzales@farmage.us",
+            "phone": "+1 (871) 593-3921",
+            "address": "705 Humboldt Street, Faywood, Oregon, 1110",
+            "about": "Minim non excepteur esse ex excepteur adipisicing occaecat sit cillum laboris dolore enim ut deserunt. Et labore fugiat consectetur incididunt commodo consectetur cupidatat consectetur do. Laborum dolor elit cillum Lorem veniam. Minim elit culpa voluptate fugiat occaecat ex enim. Aliqua commodo reprehenderit eiusmod elit proident occaecat aliqua laborum anim ullamco non duis. Eu dolor proident pariatur ut elit. Velit ex sunt ad laborum quis pariatur esse sunt.",
+            "registered": "Friday, August 15, 2014 3:21 PM",
+            "latitude": "65.464207",
+            "longitude": "-166.528258",
+            "tags": [
+                "tempor",
+                "ipsum",
+                "fugiat",
+                "ullamco",
+                "cillum"
+            ],
+            "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            ],
+            "friends": [{
+                    "id": 0,
+                    "name": "Opal Ratliff"
+                },
+                {
+                    "id": 1,
+                    "name": "Lowery Workman"
+                },
+                {
+                    "id": 2,
+                    "name": "Parks Hardy"
+                }
+            ],
+            "greeting": "Hello, Hamilton! You have 9 unread messages.",
+            "favoriteFruit": "banana"
+        },
+        {
+            "_id": "594ec1ff7ed5ac304bc70f86",
+            "index": 3,
+            "guid": "3e41e513-385a-41e0-8a57-a980d9c7263a",
+            "isActive": false,
+            "balance": "$1,215.24",
+            "picture": "http://placehold.it/32x32",
+            "age": 23,
+            "eyeColor": "brown",
+            "name": {
+                "first": "Benson",
+                "last": "Randolph"
+            },
+            "company": "NUTRALAB",
+            "email": "benson.randolph@nutralab.com",
+            "phone": "+1 (910) 449-3166",
+            "address": "935 Nassau Street, Freelandville, Colorado, 9510",
+            "about": "Aute consectetur irure Lorem dolor qui sunt officia. Eu reprehenderit nulla laborum ullamco. Incididunt velit aliquip proident et proident consectetur anim sit. Excepteur Lorem ipsum Lorem sunt ex irure qui ex dolore fugiat nulla cillum sit deserunt. Cupidatat dolor ipsum irure sint sunt ex ipsum voluptate ea. Qui eu veniam quis pariatur mollit deserunt laboris et qui.",
+            "registered": "Friday, June 13, 2014 1:04 PM",
+            "latitude": "8.809701",
+            "longitude": "80.630247",
+            "tags": [
+                "do",
+                "quis",
+                "deserunt",
+                "deserunt",
+                "proident"
+            ],
+            "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            ],
+            "friends": [{
+                    "id": 0,
+                    "name": "Frank Carter"
+                },
+                {
+                    "id": 1,
+                    "name": "Lourdes Scott"
+                },
+                {
+                    "id": 2,
+                    "name": "Hammond Harrington"
+                }
+            ],
+            "greeting": "Hello, Benson! You have 5 unread messages.",
+            "favoriteFruit": "banana"
+        },
+        {
+            "_id": "594ec1ff325849332f340edd",
+            "index": 4,
+            "guid": "99f058e4-839f-4fcb-9999-56cb3672a3e0",
+            "isActive": false,
+            "balance": "$1,981.01",
+            "picture": "http://placehold.it/32x32",
+            "age": 26,
+            "eyeColor": "green",
+            "name": {
+                "first": "Juliet",
+                "last": "Carpenter"
+            },
+            "company": "UPLINX",
+            "email": "juliet.carpenter@uplinx.tv",
+            "phone": "+1 (974) 593-2916",
+            "address": "605 Rock Street, Byrnedale, Michigan, 6001",
+            "about": "Elit culpa aute incididunt laborum Lorem consectetur sint do est do dolore. Laboris magna proident ex aute commodo laborum dolor magna dolor voluptate. Deserunt eu tempor pariatur sint esse aliqua eu velit cupidatat.",
+            "registered": "Monday, December 26, 2016 1:00 PM",
+            "latitude": "8.993484",
+            "longitude": "39.082554",
+            "tags": [
+                "cupidatat",
+                "fugiat",
+                "duis",
+                "dolor",
+                "commodo"
+            ],
+            "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            ],
+            "friends": [{
+                    "id": 0,
+                    "name": "Crosby Mendoza"
+                },
+                {
+                    "id": 1,
+                    "name": "Imogene Atkins"
+                },
+                {
+                    "id": 2,
+                    "name": "Lorene Camacho"
+                }
+            ],
+            "greeting": "Hello, Juliet! You have 9 unread messages.",
+            "favoriteFruit": "strawberry"
+        },
+        {
+            "_id": "594ec1ff5e3a85058fd4a392",
+            "index": 5,
+            "guid": "c2794bb2-2ce1-4676-8e2d-e8d58bcb112e",
+            "isActive": false,
+            "balance": "$1,114.15",
+            "picture": "http://placehold.it/32x32",
+            "age": 22,
+            "eyeColor": "blue",
+            "name": {
+                "first": "Shawna",
+                "last": "Beach"
+            },
+            "company": "ROUGHIES",
+            "email": "shawna.beach@roughies.ca",
+            "phone": "+1 (908) 528-2974",
+            "address": "944 Melrose Street, Cataract, Washington, 1011",
+            "about": "Incididunt voluptate dolor nulla velit elit elit irure sunt officia et labore nisi quis. In nisi laborum sunt aliqua laboris aliquip velit nostrud tempor ea. Pariatur Lorem sint irure ex voluptate exercitation laboris exercitation.",
+            "registered": "Tuesday, March 29, 2016 8:39 PM",
+            "latitude": "-35.329964",
+            "longitude": "140.264896",
+            "tags": [
+                "amet",
+                "occaecat",
+                "ea",
+                "nostrud",
+                "veniam"
+            ],
+            "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            ],
+            "friends": [{
+                    "id": 0,
+                    "name": "Grace White"
+                },
+                {
+                    "id": 1,
+                    "name": "Bettie Reilly"
+                },
+                {
+                    "id": 2,
+                    "name": "Chrystal Conrad"
+                }
+            ],
+            "greeting": "Hello, Shawna! You have 6 unread messages.",
+            "favoriteFruit": "apple"
+        },
+        {
+            "_id": "594ec1ffbddd2a6afff53bf9",
+            "index": 6,
+            "guid": "458a5fbf-4b4e-45ce-b0b9-a32280035098",
+            "isActive": true,
+            "balance": "$3,992.03",
+            "picture": "http://placehold.it/32x32",
+            "age": 27,
+            "eyeColor": "blue",
+            "name": {
+                "first": "Katrina",
+                "last": "Martin"
+            },
+            "company": "CODACT",
+            "email": "katrina.martin@codact.org",
+            "phone": "+1 (883) 541-2496",
+            "address": "426 Porter Avenue, Stevens, Kansas, 8665",
+            "about": "Voluptate aute do id reprehenderit Lorem consequat elit ex consectetur id. Nulla veniam ipsum eu est. In labore aliqua et velit in anim incididunt ex do fugiat fugiat. Fugiat Lorem minim enim nisi eu Lorem aliqua eu pariatur excepteur consectetur pariatur. Do tempor mollit nulla quis ea ipsum eiusmod et laborum.",
+            "registered": "Tuesday, October 7, 2014 8:33 PM",
+            "latitude": "-62.250721",
+            "longitude": "-173.234994",
+            "tags": [
+                "duis",
+                "occaecat",
+                "nisi",
+                "amet",
+                "incididunt"
+            ],
+            "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            ],
+            "friends": [{
+                    "id": 0,
+                    "name": "Levy Baxter"
+                },
+                {
+                    "id": 1,
+                    "name": "Tracie Morales"
+                },
+                {
+                    "id": 2,
+                    "name": "Sharron Cohen"
+                }
+            ],
+            "greeting": "Hello, Katrina! You have 6 unread messages.",
+            "favoriteFruit": "banana"
+        },
+        {
+            "_id": "594ec1ff3e25fc1157a25df7",
+            "index": 7,
+            "guid": "f42a6665-37e2-429e-b4d7-9f122a735cf1",
+            "isActive": false,
+            "balance": "$1,650.06",
+            "picture": "http://placehold.it/32x32",
+            "age": 28,
+            "eyeColor": "brown",
+            "name": {
+                "first": "Campos",
+                "last": "Franklin"
+            },
+            "company": "GEEKUS",
+            "email": "campos.franklin@geekus.info",
+            "phone": "+1 (930) 447-3404",
+            "address": "628 Trucklemans Lane, Mappsville, Pennsylvania, 2196",
+            "about": "Fugiat dolor non veniam aute amet amet ullamco nostrud quis dolor minim. Tempor dolor eiusmod labore dolore. Proident occaecat incididunt duis cillum est sunt deserunt magna sint.",
+            "registered": "Friday, April 3, 2015 6:55 PM",
+            "latitude": "-11.995606",
+            "longitude": "71.227027",
+            "tags": [
+                "reprehenderit",
+                "incididunt",
+                "consectetur",
+                "dolore",
+                "duis"
+            ],
+            "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            ],
+            "friends": [{
+                    "id": 0,
+                    "name": "Maldonado Osborne"
+                },
+                {
+                    "id": 1,
+                    "name": "Compton Armstrong"
+                },
+                {
+                    "id": 2,
+                    "name": "Bettye Francis"
+                }
+            ],
+            "greeting": "Hello, Campos! You have 10 unread messages.",
+            "favoriteFruit": "strawberry"
+        },
+        {
+            "_id": "594ec1ff2de107c76bf01ac6",
+            "index": 8,
+            "guid": "fb1e17db-84de-427c-9cab-d5cad90d2e1c",
+            "isActive": true,
+            "balance": "$3,642.57",
+            "picture": "http://placehold.it/32x32",
+            "age": 37,
+            "eyeColor": "brown",
+            "name": {
+                "first": "Payne",
+                "last": "Burton"
+            },
+            "company": "AQUACINE",
+            "email": "payne.burton@aquacine.biz",
+            "phone": "+1 (810) 546-3414",
+            "address": "833 Nautilus Avenue, Wawona, District Of Columbia, 6915",
+            "about": "Exercitation esse Lorem laborum veniam sunt minim proident dolore laboris nostrud. Ut labore eu pariatur esse proident. Consequat est nisi pariatur nulla aliquip dolore nulla adipisicing nisi commodo. In aute eiusmod elit culpa irure sit amet mollit esse aliquip in nisi consectetur.",
+            "registered": "Tuesday, June 6, 2017 10:15 AM",
+            "latitude": "-10.950842",
+            "longitude": "117.096865",
+            "tags": [
+                "adipisicing",
+                "amet",
+                "dolor",
+                "excepteur",
+                "ullamco"
+            ],
+            "range": [
+                0,
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9
+            ],
+            "friends": [{
+                    "id": 0,
+                    "name": "Simmons Cruz"
+                },
+                {
+                    "id": 1,
+                    "name": "Young Wiggins"
+                },
+                {
+                    "id": 2,
+                    "name": "Lenora Newton"
+                }
+            ],
+            "greeting": "Hello, Payne! You have 9 unread messages.",
+            "favoriteFruit": "strawberry"
+        }
+    ]
+}
