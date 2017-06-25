4066acdb68c02008d0633b7a26ba1af0ff7af563
mauricionr <mauricioskateboard@gmail.com>
2017-06-25 12:46:55 -0300

    refactor

--- app.js
+++ app.js
@@ -1,49 +1,40 @@
-/*
-Copyright 2017 Google Inc.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
-var express = require('express');
-var formidable = require('formidable');
-
-var app = express();
-
-var skuToSizeAndPrice = require('./data/data.js').skuToSizeAndPrice;
-
-const data = {
-    items: require('./data/data.js').items
-}
+const express = require('express');
+const formidable = require('formidable');
+const app = express();
+const skuToSizeAndPrice = require('./data/data.js').skuToSizeAndPrice;
 
 app.get('/shirts/sizesAndPrices', function(req, res) {
-    var sku = req.query.sku;
-    var response = {};
+    let sku = req.query.sku;
+    let response = {};
     response[sku] = skuToSizeAndPrice[sku];
     setTimeout(() => res.json(response), 1000); // Simulate network delay.
 });
 
 app.get('/search', function(req, res) {
-    console.log(req.query)
+    let data = { item: [] }
+    if (req.query) {
+        data.items = require('./data/data.js').items.filter(item => findIndex(item, req))
+    } else {
+        data.items = require('./data/data.js').items;
+    }
+
     res.status(200).json(data);
 })
 
+const findIndex = (item, req) => {
+    return (
+        item.name.first.indexOf(req.query) > -1 ||
+        item.name.last.indexOf(req.query) > -1 ||
+        item.about.indexOf(req.query) > -1
+    )
+}
 app.post('/shirts/addToCart', function(req, res) {
     // Necessary for AMP CORS security protocol.
     // @see https://github.com/ampproject/amphtml/blob/master/spec/amp-cors-requests.md
     res.setHeader('AMP-Access-Control-Allow-Source-Origin',
         'http://localhost:3000');
 
-    var form = new formidable.IncomingForm();
+    let form = new formidable.IncomingForm();
     form.parse(req, function(err, fields) {
         if (fields.color && fields.size && fields.quantity) {
             res.status(200).json(fields);
